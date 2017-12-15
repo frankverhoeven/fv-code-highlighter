c32d8bd91acac2a8dd05ffbd7be5cb691e7b0b23
Frank Verhoeven <frankverhoeven@me.com>
2017-12-15 23:32:47 +0100

    Add valid method chars

--- src/Highlighter/AbstractHighlighter.php
+++ src/Highlighter/AbstractHighlighter.php
@@ -42,6 +42,16 @@ abstract class AbstractHighlighter implements HighlighterInterface
     ];
 
     /**
+     * @var array List of valid method name chars
+     */
+    public static $methodChars = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '_',
+    ];
+
+    /**
      * @var Block[]|Key[]
      */
     protected $elements;
