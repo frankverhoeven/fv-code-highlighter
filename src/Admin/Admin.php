072f73ced19ed73c3ef47b4eea8003851cb9efe5
Frank Verhoeven <frankverhoeven@me.com>
2018-02-16 22:52:46 +0100

    Use Settings API

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -40,11 +40,11 @@ class Admin
 	{
         if ($this->optionsPageHook != $hook) return;
 
-        wp_register_style('fvch-admin-css', plugins_url('public/css/admin.min.css', dirname(__FILE__, 2)), false, '1.1');
-        wp_enqueue_style('fvch-admin-css');
-        wp_enqueue_style('farbtastic');
+        \wp_register_style('fvch-admin-css', \plugins_url('public/css/admin.min.css', \dirname(__FILE__, 2)), false, '1.1');
+        \wp_enqueue_style('fvch-admin-css');
+        \wp_enqueue_style('farbtastic');
 
-        wp_enqueue_script('fvch-admin-js', plugins_url('public/js/admin.min.js', dirname(__FILE__, 2)), ['jquery', 'farbtastic'], '1.0');
+        \wp_enqueue_script('fvch-admin-js', \plugins_url('public/js/admin.min.js', \dirname(__FILE__, 2)), ['jquery', 'farbtastic'], '1.0');
 	}
 
     /**
@@ -53,27 +53,12 @@ class Admin
      */
 	public function adminMenu()
 	{
-		$this->optionsPageHook = add_theme_page(
+		$this->optionsPageHook = \add_theme_page(
 			__('FV Code Highlighter Options', 'fvch'),
 			__('Code Highlighter', 'fvch'),
 			'edit_themes',
 			'fvch-options',
-			[$this, 'optionsPage']
+			[new Options($this->options), 'page']
 		);
 	}
-
-    /**
-     * Display the admin page.
-     *
-     */
-    public function optionsPage()
-    {
-        $optionsPage = new Options($this->options);
-
-        if ('POST' == $_SERVER['REQUEST_METHOD']) {
-            $optionsPage->updateOptions();
-        }
-
-        $optionsPage->displayOptions();
-    }
 }
--- src/Admin/Options.php
+++ src/Admin/Options.php
@@ -17,153 +17,173 @@ class Options
     protected $options;
 
     /**
-     * __construct()
-     *
      * @param PluginOptions $options
      */
     public function __construct(PluginOptions $options)
     {
-        if (!current_user_can('edit_themes')) {
-            wp_die(__('You do not have sufficient permissions to manage options for this site.'));
-        }
-
         $this->options = $options;
+        $this->init();
     }
 
-    /**
-     * Update the options.
-     *
-     * @return $this
-     */
-    public function updateOptions()
+    public function init(): void
     {
-        check_admin_referer('fvch');
-
-        foreach ($this->options->getDefaultOptions() as $key => $default) {
-            if (isset($_POST[ $key ])) {
-                $this->options->updateOption($key, $_POST[ $key ]);
-            }
+        if (!\current_user_can('edit_themes')) {
+            \wp_die(__('You do not have sufficient permissions to manage options for this site.'));
         }
 
-        if (!isset($_POST['fvch-line-numbers'])) {
-            $this->options->updateOption('fvch-line-numbers', '0');
-        }
-        if (!isset($_POST['fvch-toolbox'])) {
-            $this->options->updateOption('fvch-toolbox', '0');
-        }
+        // Font
+        \add_settings_section('fvch_setting_font_section', \__('Font', 'fvch'), [$this, 'fontSection'], 'fvch-options');
 
-        add_settings_error('fvch-options', 'fvch-updated', __('Your settings have been updated', 'fvch'), 'updated');
+        \add_settings_field('fvch-font-family', \__('Font Family', 'fvch'), [$this, 'fontFamily'], 'fvch-options', 'fvch_setting_font_section');
+        \register_setting('fvch-options', 'fvch-font-family', ['type' => 'string', 'sanitize_callback' => 'sanitize_text_field']);
 
-        return $this;
+        \add_settings_field('fvch-font-size', \__('Font Size', 'fvch'), [$this, 'fontSize'], 'fvch-options', 'fvch_setting_font_section');
+        \register_setting('fvch-options', 'fvch-font-size', ['type' => 'number', 'sanitize_callback' => 'sanitize_text_field']);
+
+        // Background
+        \add_settings_section('fvch_setting_background_section', \__('Background', 'fvch'), [$this, 'backgroundSection'], 'fvch-options');
+
+        \add_settings_field('fvch-background', \__('Background', 'fvch'), [$this, 'background'], 'fvch-options', 'fvch_setting_background_section');
+        \register_setting('fvch-options', 'fvch-background', ['type' => 'string', 'sanitize_callback' => 'sanitize_text_field']);
+        \register_setting('fvch-options', 'fvch-background-custom', ['type' => 'string', 'sanitize_callback' => 'sanitize_text_field']);
+
+        // Utilities
+        \add_settings_section('fvch_setting_utility_section', \__('Utilities', 'fvch'), [$this, 'utilitySection'], 'fvch-options');
+
+        \add_settings_field('fvch-line-numbers', \__('Line Numbers', 'fvch'), [$this, 'lineNumbers'], 'fvch-options', 'fvch_setting_utility_section');
+        \register_setting('fvch-options', 'fvch-line-numbers', ['type' => 'boolean', 'sanitize_callback' => 'boolval']);
+
+        \add_settings_field('fvch-toolbox', \__('Toolbox', 'fvch'), [$this, 'toolbox'], 'fvch-options', 'fvch_setting_utility_section');
+        \register_setting('fvch-options', 'fvch-toolbox', ['type' => 'boolean', 'sanitize_callback' => 'boolval']);
+
+        \add_settings_field('fvch-dark-mode', \__('Dark Mode', 'fvch'), [$this, 'darkMode'], 'fvch-options', 'fvch_setting_utility_section');
+        \register_setting('fvch-options', 'fvch-dark-mode', ['type' => 'boolean', 'sanitize_callback' => 'boolval']);
     }
 
-    /**
-     * Display the options page.
-     *
-     * @return void
-     */
-    public function displayOptions()
+    public function page(): void
     {
-?>
-<div class="wrap fvch-options-page">
-	<h2><?php _e('FV Code Highlighter Options'); ?></h2>
-	<?php settings_errors(); ?>
+        ?>
+        <div class="wrap">
+            <h2><?php \_e('FV Code Highlighter Options', 'fvch'); ?></h2>
+            <?php \settings_errors(); ?>
 
-    <div class="fvch-support">
-        <a href="https://www.paypal.me/FrankVerhoeven" class="button button-primary">
-            <?php _e('Support this plugin', 'fvch'); ?>
-        </a>
-    </div>
+            <div class="fvch-support">
+                <a href="https://www.paypal.me/FrankVerhoeven" class="button button-primary">
+                    <?php \_e('Support this plugin', 'fvch'); ?>
+                </a>
+            </div>
 
-	<form method="post" action="themes.php?page=fvch-options">
-		<table class="form-table">
-			<tr valign="top">
-				<th scope="row"><?php _e('Font', 'fvch'); ?></th>
-				<td>
-					<label>
-						<input type="radio" name="fvch-font-family" value="Andale Mono" id="fvch-font-family_0" <?php checked('Andale Mono', $this->options->getOption('fvch-font-family')); ?> />
-						<span style="font-family: 'Andale Mono', 'Courier New', Courier, monospace;">Andale Mono</span>
-					</label><br />
-					<label>
-						<input type="radio" name="fvch-font-family" value="Courier" id="fvch-font-family_1" <?php checked('Courier', $this->options->getOption('fvch-font-family')); ?> />
-						<span style="font-family: Courier, 'Courier New', Courier, monospace;">Courier</span>
-					</label><br />
-					<label>
-						<input type="radio" name="fvch-font-family" value="Courier New" id="fvch-font-family_2" <?php checked('Courier New', $this->options->getOption('fvch-font-family')); ?> />
-						<span style="font-family: 'Courier New', Courier, monospace;">Courier New</span>
-					</label><br />
-					<label>
-						<input type="radio" name="fvch-font-family" value="Menlo" id="fvch-font-family_3" <?php checked('Menlo', $this->options->getOption('fvch-font-family')); ?> />
-						<span style="font-family: 'Menlo', 'Courier New', Courier, monospace;">Menlo</span>
-					</label><br />
-					<label>
-						<input type="radio" name="fvch-font-family" value="Monaco" id="fvch-font-family_4" <?php checked('Monaco', $this->options->getOption('fvch-font-family')); ?> />
-						<span style="font-family: 'Monaco', 'Courier New', Courier, monospace;">Monaco</span>
-					</label>
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row"><label for="fvch-font-size"><?php _e('Font Size', 'fvch'); ?></label></th>
-				<td>
-					<select name="fvch-font-size" id="fvch-font-size">
-					<?php for ($i = .5; $i < 2.1; $i += .1) : ?>
-						<option <?php selected($this->options->getOption('fvch-font-size'), $i); ?> value="<?php echo $i; ?>"><?php echo $i; ?></option>
-					<?php endfor; ?>
-					</select>
-					<code>em</code>
-                    <p class="description"><?php _e('Using a font-size greater than 17px in combination with the notepaper background might cause display issues.', 'fvch'); ?></p>
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row"><?php _e('Background', 'fvch'); ?></th>
-				<td>
-					<label class="fvch-background-option description notepaper">
-						<input type="radio" name="fvch-background" value="notepaper" id="fvch-background_0" <?php checked('notepaper', $this->options->getOption('fvch-background')); ?> />
-						<span class="fvch-background-example fvch-notepaper"></span>
-						<span><?php _e('Notepaper', 'fvch'); ?></span>
-					</label>
-					<label class="fvch-background-option description white">
-						<input type="radio" name="fvch-background" value="white" id="fvch-background_1" <?php checked('white', $this->options->getOption('fvch-background')); ?> />
-						<span class="fvch-background-example fvch-white"></span>
-						<span><?php _e('White', 'fvch'); ?></span>
-					</label>
-					<label class="fvch-background-option description custom">
-						<input type="radio" name="fvch-background" value="custom" id="fvch-background_3" <?php checked('custom', $this->options->getOption('fvch-background')); ?> />
-						<span class="fvch-background-example fvch-custom" style="background-color: <?php esc_attr_e($this->options->getOption('fvch-background-custom')); ?>;">
-                            <span id="fvch-colorpicker"></span>
-                        </span>
-                        <input type="hidden" name="fvch-background-custom" id="fvch-background-custom" value="<?php esc_attr_e($this->options->getOption('fvch-background-custom')); ?>" />
-						<span><?php _e('Custom', 'fvch'); ?></span>
-					</label>
+            <form action="<?= \admin_url('options.php'); ?>" method="post">
+                <?php \settings_fields('fvch-options'); ?>
+                <?php \do_settings_sections('fvch-options'); ?>
 
-					<br style="clear: both;" />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row"><?php _e('Line Numbers', 'fvch'); ?></th>
-				<td>
-					<label>
-						<input type="checkbox" name="fvch-line-numbers" id="fvch-line-numbers" <?php checked('1', $this->options->getOption('fvch-line-numbers')); ?> value="1" />
-						<?php _e('Check to enable line numbers.', 'fvch'); ?>
-					</label>
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row"><?php _e('Toolbox', 'fvch'); ?></th>
-				<td>
-					<label>
-						<input type="checkbox" name="fvch-toolbox" id="fvch-toolbox" <?php checked('1', $this->options->getOption('fvch-toolbox')); ?> value="1" />
-						<?php _e('Check to enable the toolbox.', 'fvch'); ?>
-					</label>
-				</td>
-			</tr>
-		</table>
+                <?php \submit_button(); ?>
+            </form>
+        </div>
+        <?php
+    }
 
-		<?php wp_nonce_field('fvch'); ?>
+    public function fontSection(): void
+    {}
 
-		<?php submit_button(); ?>
-	</form>
-</div>
-<?php
+    public function fontFamily(): void
+    {
+        ?>
+        <label>
+            <input type="radio" name="fvch-font-family" value="Andale Mono" id="fvch-font-family_0" <?php \checked('Andale Mono', $this->options->getOption('fvch-font-family')); ?>>
+            <span style="font-family: 'Andale Mono', 'Courier New', Courier, monospace;">Andale Mono</span>
+        </label><br>
+        <label>
+            <input type="radio" name="fvch-font-family" value="Courier" id="fvch-font-family_1" <?php \checked('Courier', $this->options->getOption('fvch-font-family')); ?>>
+            <span style="font-family: Courier, 'Courier New', Courier, monospace;">Courier</span>
+        </label><br>
+        <label>
+            <input type="radio" name="fvch-font-family" value="Courier New" id="fvch-font-family_2" <?php \checked('Courier New', $this->options->getOption('fvch-font-family')); ?>>
+            <span style="font-family: 'Courier New', Courier, monospace;">Courier New</span>
+        </label><br>
+        <label>
+            <input type="radio" name="fvch-font-family" value="Menlo" id="fvch-font-family_3" <?php \checked('Menlo', $this->options->getOption('fvch-font-family')); ?>>
+            <span style="font-family: 'Menlo', 'Courier New', Courier, monospace;">Menlo</span>
+        </label><br>
+        <label>
+            <input type="radio" name="fvch-font-family" value="Monaco" id="fvch-font-family_4" <?php \checked('Monaco', $this->options->getOption('fvch-font-family')); ?>>
+            <span style="font-family: 'Monaco', 'Courier New', Courier, monospace;">Monaco</span>
+        </label>
+        <?php
+    }
+
+    public function fontSize(): void
+    {
+        ?>
+        <select name="fvch-font-size" id="fvch-font-size">
+            <?php for ($i = .5; $i < 2.1; $i += .1) : ?>
+                <option <?php \selected($this->options->getOption('fvch-font-size'), $i); ?> value="<?= $i; ?>"><?= $i; ?></option>
+            <?php endfor; ?>
+        </select>
+        <label for="fvch-font-size"><code>em</code></label>
+        <p class="description"><?php \_e('Using a font-size greater than 17px in combination with the notepaper background might cause display issues.', 'fvch'); ?></p>
+        <?php
+    }
+
+    public function backgroundSection(): void
+    {}
+
+    public function background(): void
+    {
+        ?>
+        <label class="fvch-background-option description notepaper">
+            <input type="radio" name="fvch-background" value="notepaper" id="fvch-background_0" <?php \checked('notepaper', $this->options->getOption('fvch-background')); ?>>
+            <span class="fvch-background-example fvch-notepaper"></span>
+            <span><?php \_e('Notepaper', 'fvch'); ?></span>
+        </label>
+        <label class="fvch-background-option description white">
+            <input type="radio" name="fvch-background" value="white" id="fvch-background_1" <?php \checked('white', $this->options->getOption('fvch-background')); ?>>
+            <span class="fvch-background-example fvch-white"></span>
+            <span><?php \_e('White', 'fvch'); ?></span>
+        </label>
+        <label class="fvch-background-option description custom">
+            <input type="radio" name="fvch-background" value="custom" id="fvch-background_3" <?php \checked('custom', $this->options->getOption('fvch-background')); ?>>
+            <span class="fvch-background-example fvch-custom" style="background-color: <?php \esc_attr_e($this->options->getOption('fvch-background-custom')); ?>;">
+                <span id="fvch-colorpicker"></span>
+            </span>
+            <input type="hidden" name="fvch-background-custom" id="fvch-background-custom" value="<?php \esc_attr_e($this->options->getOption('fvch-background-custom')); ?>">
+            <span><?php \_e('Custom', 'fvch'); ?></span>
+        </label>
+
+        <br style="clear: both;">
+        <?php
+    }
+
+    public function utilitySection(): void
+    {}
+
+    public function lineNumbers(): void
+    {
+        ?>
+        <label>
+            <input type="checkbox" name="fvch-line-numbers" id="fvch-line-numbers" <?php \checked('1', $this->options->getOption('fvch-line-numbers')); ?> value="1">
+            <?php \_e('Check to enable line numbers.', 'fvch'); ?>
+        </label>
+        <?php
+    }
+
+    public function toolbox(): void
+    {
+        ?>
+        <label>
+            <input type="checkbox" name="fvch-toolbox" id="fvch-toolbox" <?php \checked('1', $this->options->getOption('fvch-toolbox')); ?> value="1">
+            <?php \_e('Check to enable the toolbox.', 'fvch'); ?>
+        </label>
+        <?php
+    }
+
+    public function darkMode(): void
+    {
+        ?>
+        <label>
+            <input type="checkbox" name="fvch-dark-mode" id="fvch-dark-mode" <?php \checked('1', $this->options->getOption('fvch-dark-mode')); ?> value="1">
+            <?php \_e('Check to use dark mode.', 'fvch'); ?>
+        </label>
+        <?php
     }
 }
